dca4d5f1ae5495c1d126bf461c4b2c6fb069335d
jsakamoto <junichi.jp@gmail.com>
2017-05-19 23:32:32 +0900

    Keep compatibility for .NET Core.

---
+++ WhoisClient.NETCore/Thread.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Whois.NET
+{
+    internal static class Thread
+    {
+
+        public static void Sleep(int milliseconds)
+        {
+            Task.Delay(milliseconds).Wait();
+        }
+    }
+}
