74c100ffa4bc24eacb494d8ecf991ac61ce0e1b7
jsakamoto <junichi.jp@gmail.com>
2018-07-20 23:12:57 +0900

    Fix: poor algorithm of detection referer server.

--- WhoisClient.NET.Test.Core20/IpTests.cs
+++ WhoisClient.NET.Test.Core20/IpTests.cs
@@ -8,9 +8,9 @@ namespace WhoisClient_NET.Test
     {
         [Theory]
         [InlineData(@"4.4.4.4", @"Level 3 Communications, Inc. LVLT-STATIC-4-4-16 (NET-4-4-0-0-1)", @"4.4.0.0-4.4.255.255")]
-        [InlineData(@"65.100.170.169", @"Qwest Communications Company, LLC QWEST-INET-115 (NET-65-100-0-0-1)", @"65.100.0.0-65.103.255.255")]
-        [InlineData(@"108.234.177.20", @"AT&T Internet Services", @"108.192.0.0-108.255.255.255")]
-        [InlineData(@"190.190.132.64", @"Prima S.A.", @"190.0.0.0-190.1.255.255")]
+        [InlineData(@"65.100.170.169", @"Alana Scheffield USW-FFCF (NET-65-100-170-168-1)", @"65.100.170.168-65.100.170.175")]
+        [InlineData(@"108.234.177.20", @"AT&T Corp.", @"108.192.0.0-108.255.255.255")]
+        [InlineData(@"190.190.132.64", @"Telecom Argentina S.A.", @"190.0.0.0-190.1.255.255")]
         [InlineData(@"31.116.94.96", @"EE route", @"31.64.0.0-31.127.255.255")]
         public void WhoisClientTest(string ip, string expectedOrgName, string expectedAddressRange)
         {
@@ -21,9 +21,9 @@ namespace WhoisClient_NET.Test
 
         [Theory]
         [InlineData(@"4.4.4.4", @"Level 3 Communications, Inc. LVLT-STATIC-4-4-16 (NET-4-4-0-0-1)", @"4.4.0.0-4.4.255.255")]
-        [InlineData(@"65.100.170.169", @"Qwest Communications Company, LLC QWEST-INET-115 (NET-65-100-0-0-1)", @"65.100.0.0-65.103.255.255")]
-        [InlineData(@"108.234.177.20", @"AT&T Internet Services", @"108.192.0.0-108.255.255.255")]
-        [InlineData(@"190.190.132.64", @"Prima S.A.", @"190.0.0.0-190.1.255.255")]
+        [InlineData(@"65.100.170.169", @"Alana Scheffield USW-FFCF (NET-65-100-170-168-1)", @"65.100.170.168-65.100.170.175")]
+        [InlineData(@"108.234.177.20", @"AT&T Corp.", @"108.192.0.0-108.255.255.255")]
+        [InlineData(@"190.190.132.64", @"Telecom Argentina S.A.", @"190.0.0.0-190.1.255.255")]
         [InlineData(@"31.116.94.96", @"EE route", @"31.64.0.0-31.127.255.255")]
         public async Task WhoisClientAsyncTest(string ip, string expectedOrgName, string expectedAddressRange)
         {
