16566b2caa75b1162eff37190ba5f350e59f889b
jsakamoto <junichi.jp@gmail.com>
2025-03-10 22:11:59 +0900

    Add a test case that simulates interrupting multi-byte encoded string response.

---
+++ WhoisClient.NET.Test/Internals/MockServer.cs
@@ -0,0 +1,74 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WhoisClient_NET.Test.Internals
+{
+    /// <summary>
+    /// A mock server for testing purposes.
+    /// </summary>
+    internal class MockServer : IDisposable
+    {
+        private readonly TcpListener _listener;
+
+        private readonly CancellationTokenSource _cts = new CancellationTokenSource();
+
+        /// <summary>
+        /// Gets the port number the server is listening on.
+        /// </summary>
+        public int Port { get; }
+
+        private readonly Func<TcpClient, CancellationToken, Task> _handleClientAsync;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MockServer"/> class.
+        /// </summary>
+        /// <param name="handleClientAsync">A function to respond mocking data to a client.</param>
+        public MockServer(Func<TcpClient, CancellationToken, Task> handleClientAsync)
+        {
+            this._listener = new TcpListener(IPAddress.Loopback, 0);
+            this._listener.Start();
+            this.Port = ((IPEndPoint)this._listener.LocalEndpoint).Port;
+            Task.Run(() => this.AcceptClientsAsync(this._cts.Token));
+            this._handleClientAsync = handleClientAsync;
+        }
+
+        /// <summary>
+        /// Accepts incoming client connections asynchronously.
+        /// </summary>
+        /// <param name="token">A cancellation token to cancel the operation.</param>
+        private async Task AcceptClientsAsync(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    var client = await this._listener.AcceptTcpClientAsync();
+                    _ = Task.Run(() => this.HandleClientAsync(client, token));
+                }
+            }
+            catch (ObjectDisposedException) when (token.IsCancellationRequested) { }
+        }
+
+        /// <summary>
+        /// Handles a client connection asynchronously.
+        /// </summary>
+        /// <param name="client">The client to handle.</param>
+        /// <param name="token">A cancellation token to cancel the operation.</param>
+        private async Task HandleClientAsync(TcpClient client, CancellationToken token)
+        {
+            using (client)
+            {
+                await this._handleClientAsync(client, token);
+            }
+        }
+
+        /// <summary>
+        /// Releases all resources used by the <see cref="MockServer"/>.
+        /// </summary>
+        public void Dispose()
+        {
+            this._cts.Cancel();
+            this._listener.Stop();
+        }
+    }
+}
---
+++ WhoisClient.NET.Test/IrregularCaseTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using NUnit.Framework;
+using Whois.NET;
+using WhoisClient_NET.Test.Internals;
+
+namespace WhoisClient_NET.Test
+{
+    public class IrregularCaseTests
+    {
+        [Test]
+        public async Task Interrupt_Middle_of_MultiBytesEncodedStrings_Test()
+        {
+#if NETCOREAPP
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+#endif
+            // Given: a server that sends a multi-byte encoded string
+            using var server = new MockServer(async (client, token) =>
+            {
+                var responseBytes = Encoding.GetEncoding("iso-2022-jp").GetBytes("こちらはテスト文字列である");
+                var firstHalfLength = responseBytes.Length / 2;
+                var lastHalfLength = responseBytes.Length - firstHalfLength;
+
+                // Send the first half of the response
+                var stream = client.GetStream();
+                await stream.WriteAsync(responseBytes, 0, firstHalfLength, token);
+                await stream.FlushAsync(token);
+                await Task.Delay(100, token);
+
+                // Send the left of the response
+                await stream.WriteAsync(responseBytes, firstHalfLength, lastHalfLength, token);
+                await stream.FlushAsync(token);
+                await Task.Delay(100, token);
+            });
+
+            // When: a query is made to the server
+            var options = new WhoisQueryOptions { Server = "localhost", Port = server.Port, Encoding = Encoding.GetEncoding("iso-2022-jp") };
+            var response = await WhoisClient.RawQueryAsync("example.jp", options);
+
+            // Then: the response should be the same as the server's response
+            response.Is("こちらはテスト文字列である");
+        }
+    }
+}
